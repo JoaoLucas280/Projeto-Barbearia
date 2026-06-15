0a94305f98140d06ca2c1e553bbf4a5d90049417
João Lucas Topazio <joaolucas280408@gmail.com>
2026-06-15 08:55:18 -0300

    chore: estrutura inicial do projeto com Hello World

---
+++ src/test/java/com/JoaoLucas/Sistema/Barbearia/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JoaoLucas.Sistema.Barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
