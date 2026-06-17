c82e396fc1eada5a7a0c552f6691ac48a0e6fc55
João Lucas Topazio <joaolucas280408@gmail.com>
2026-06-17 16:39:46 -0300

    fix: removed avaliacao from Barbeiro

--- src/main/java/com/JoaoLucas/Sistema/Barbearia/entity/Barbeiro.java
+++ src/main/java/com/JoaoLucas/Sistema/Barbearia/entity/Barbeiro.java
@@ -24,28 +24,20 @@ public class Barbeiro {
     private String especialidade;
     @Column
     private String descricao;
-
-    @Column(nullable = false)
-    private Double avaliacao;
     @Column
     private LocalTime horario_inicio;
     @Column
     private LocalTime horario_fim;
 
-    public void setHorario_inicio() {
-        horario_inicio = LocalTime.now();
-        horario_fim = horario_inicio.plusHours(1);
-    }
-
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Barbeiro barbeiro = (Barbeiro) o;
-        return Objects.equals(getId(), barbeiro.getId()) && Objects.equals(getNome(), barbeiro.getNome()) && Objects.equals(getEspecialidade(), barbeiro.getEspecialidade()) && Objects.equals(getDescricao(), barbeiro.getDescricao()) && Objects.equals(getAvaliacao(), barbeiro.getAvaliacao()) && Objects.equals(getHorario_inicio(), barbeiro.getHorario_inicio()) && Objects.equals(getHorario_fim(), barbeiro.getHorario_fim());
+        return Objects.equals(getId(), barbeiro.getId()) && Objects.equals(getNome(), barbeiro.getNome()) && Objects.equals(getEspecialidade(), barbeiro.getEspecialidade()) && Objects.equals(getDescricao(), barbeiro.getDescricao()) && Objects.equals(getHorario_inicio(), barbeiro.getHorario_inicio()) && Objects.equals(getHorario_fim(), barbeiro.getHorario_fim());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getNome(), getEspecialidade(), getDescricao(), getAvaliacao(), getHorario_inicio(), getHorario_fim());
+        return Objects.hash(getId(), getNome(), getEspecialidade(), getDescricao(), getHorario_inicio(), getHorario_fim());
     }
 }
